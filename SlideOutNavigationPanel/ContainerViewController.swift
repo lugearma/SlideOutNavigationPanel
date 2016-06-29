9e98aa696a98ba6a087bb16c64029df47cd73903
Luis Arias <lugearma@gmail.com>
2016-06-29 10:37:43 -0500

    Added containerViewController

--- SlideOutNavigationPanel/ContainerViewController.swift
+++ SlideOutNavigationPanel/ContainerViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2016 Luis Arias. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import QuartzCore
+
+class ContainerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+private extension UIStoryboard {
+    
+    class func mainStoryboard() -> UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+    }
+    
+    class func leftViewController() -> SidePanelViewController {
+        return (mainStoryboard().instantiateViewControllerWithIdentifier("LeftViewController") as? SidePanelViewController)!
+    }
+    
+    class func centerViewController() -> CenterViewController {
+        return (mainStoryboard().instantiateViewControllerWithIdentifier("CenterViewController") as? CenterViewController)!
+    }
+}
