9c271107e08d14fbc419ab0be28a9afa4013630a
Luis Arias <lugearma@gmail.com>
2016-06-29 11:17:35 -0500

    Added entry point

--- SlideOutNavigationPanel/ContainerViewController.swift
+++ SlideOutNavigationPanel/ContainerViewController.swift
@@ -11,8 +11,45 @@ import QuartzCore
 
 class ContainerViewController: UIViewController {
     
+    var centerNavigationViewController: UINavigationController!
+    var centerViewController: CenterViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.centerViewController = UIStoryboard.centerViewController()
+        self.centerViewController.delegate = self
+        
+        self.centerNavigationViewController = UINavigationController(rootViewController: self.centerViewController)
+        view.addSubview(self.centerNavigationViewController.view)
+        addChildViewController(self.centerNavigationViewController)
+        self.centerNavigationViewController.didMoveToParentViewController(self)
+    }
+}
+
+extension ContainerViewController: CenterViewControllerDelegate {
+    func toggleLeftPanel() {
+        
+    }
+    
+    func toggleRightPanel() {
+        
+    }
+    
+    func addLeftPanelViewController() {
+        
+    }
+    
+    func addRightPanelViewController() {
+        
+    }
+    
+    func animateLeftPanel() {
+        
+    }
+    
+    func animateRightPanel() {
+        
     }
 }
 
@@ -23,6 +60,10 @@ private extension UIStoryboard {
     }
     
     class func leftViewController() -> SidePanelViewController {
+        return (mainStoryboard().instantiateViewControllerWithIdentifier("LeftViewController") as? SidePanelViewController)!
+    }
+    
+    class func rightViewController() -> SidePanelViewController {
         return (mainStoryboard().instantiateViewControllerWithIdentifier("LeftViewController") as? SidePanelViewController)!
     }
     
